7d04fe2c2cd93b8563bbb1adb3f2a0cc36a99c02
KTHigh <kthigh@comcast.net>
2018-09-06 16:54:20 -0400

    update

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-/*//function declaration
-function newFunction() {}
-//function  expression(ends in semicolon)
-var newFunction = function() {};
-//expression denotes/produces a value
-1+3;
-var a = 2;
-return true;
-//calling or invoking a function
-alert();
-newfunction(param1, param2);
-//assign a variable
-var a = true;
-//method declaration (vs. function)
-var obj = {
-	thisIsAMethod: function() {	}
-}
-//access a method
-obj.thisIsAMethod()
-*/
-
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
